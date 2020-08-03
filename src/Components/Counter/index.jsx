f759dbe4b4ef8b74c006736341975b10070ed77e
userName <gongl3730@gmail.com>
2020-08-03 15:02:22 +0800

    [React-Counter][Bruno] implemnt: add a counter

---
+++ src/Components/Counter/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            number: 0
+        }
+    }
+
+    increase=() => {
+        this.setState((prevState) => ({
+            number: prevState.number + 1
+        }))
+    }
+
+    decrease=() => {
+        this.setState((prevState) => ({
+            number: prevState.number - 1
+        }))
+    }
+
+    render() {
+        return (
+            <h2>
+            <button onClick={this.increase}>+</button>
+            <mark>{this.state.number}</mark>
+            <button onClick={this.decrease}>-</button>
+            </h2>
+        )
+    }
+ 
+}
+
+export default Counter
